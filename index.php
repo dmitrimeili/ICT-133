7a3e211d2c1424d867e3f7f8cfa997141176bb6e
dmitrimeili <47849645+dmitrimeili@users.noreply.github.com>
2019-11-20 16:57:29 +0100

    debut

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Hoy</h1>
+<?php
+/**
+ *
+ */
+echo "Beerthe;"
+?>
+</body>
+</html>
+
