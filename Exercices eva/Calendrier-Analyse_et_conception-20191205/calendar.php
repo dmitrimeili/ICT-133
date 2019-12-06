c3e662475fa68310e6f5e232153f8a557912f706
dmitrimeili <47849645+dmitrimeili@users.noreply.github.com>
2019-12-06 15:01:02 +0100

    wip_exCalendrier

--- Exercices eva/Calendrier-Analyse_et_conception-20191205/calendar.php
+++ Exercices eva/Calendrier-Analyse_et_conception-20191205/calendar.php
@@ -7,7 +7,9 @@
 </head>
 <body>
 <?php
+$now =time();
 $month = $_GET["month"];
+$month = date("M", mktime(0, 0, 0, $month, 0, 0));
 $year = $_GET["year"];
 echo "<div class=\"month\">
     <ul>
@@ -20,14 +22,17 @@ for ($i = 0; $i < count($weekdays); $i++) {
     echo "<li>$weekdays[$i]</li>";
 }
 echo "</ul>";
-echo"<ul class=\"days\">";
-for($i=1;$i<32;$i++){
+echo "<ul class=\"days\">";
+for ($i = 1; $i < 32; $i++) {
 
-    echo "<li>$i</li>";
+    echo "<li>";
+    if($i==date("j",$now)){
+        echo"<span class='active'>";
+    }
+    echo"$i</li>";
 }
 echo "</ul>";
 ?>
-
 
 
 </ul>
