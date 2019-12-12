2177511f216e086b7c89874c2c0d2ad0eac9637f
dmitrimeili <47849645+dmitrimeili@users.noreply.github.com>
2019-12-12 12:11:24 +0100

    ex calendrier wip

--- Exercices eva/Calendrier-Analyse_et_conception-20191205/calendar.php
+++ Exercices eva/Calendrier-Analyse_et_conception-20191205/calendar.php
@@ -7,9 +7,9 @@
 </head>
 <body>
 <?php
-$now =time();
+$now = time();
 $month = $_GET["month"];
-$month = date("M", mktime(0, 0, 0, $month, 0, 0));
+$month = date("M", mktime(0, 0, 0, $month + 1, 0, 0));
 $year = $_GET["year"];
 echo "<div class=\"month\">
     <ul>
@@ -23,13 +23,18 @@ for ($i = 0; $i < count($weekdays); $i++) {
 }
 echo "</ul>";
 echo "<ul class=\"days\">";
+$dayweek = date("N", strtotime("first day of this month"));
+$prev = date("d", strtotime("last day of last month", strtotime("now")));
+for ($i = $prev - $dayweek + 2; $i <= $prev; $i++) {
+    echo "<li>$i</li>";
+}
 for ($i = 1; $i < 32; $i++) {
 
     echo "<li>";
-    if($i==date("j",$now)){
-        echo"<span class='active'>";
+    if ($i == date("j", $now)) {
+        echo "<span class='active'>";
     }
-    echo"$i</li>";
+    echo "$i</li>";
 }
 echo "</ul>";
 ?>
